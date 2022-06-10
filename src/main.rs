6e1dea135fcfc981984f31a38f53399a34186c6c
Austin Theriot <austinmtheriot@gmail.com>
2022-06-09 20:02:22 -0500

    play sine wave with rodio

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,26 @@
-use rodio::Sink;
-use rodio::source::SineWave;
-use rodio::{source::Source, Decoder, OutputStream};
-use std::fs::File;
-use std::io::BufReader;
-use std::time::Duration;
+use rodio::{Sink};
+use std::f32::consts::PI;
+
+const SAMPLE_RATE: usize = 44100;
+const SECONDS_OF_PLAYBACK: usize = 5;
+
+fn get_sample_at_frequency(frame: usize, frequency: f32,) -> f32 {
+    f32::sin((frame as f32 * frequency * PI * 2.) / SAMPLE_RATE as f32)
+}
 
 fn main() {
-    // Get a output stream handle to the default physical sound device
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("src/pater_emon.mp3").unwrap());
-    // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
-    // Play the sound directly on the device
-    stream_handle
-        .play_raw(source.convert_samples())
-        .expect("Should play raw audio");
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let mut buffer_vec = Vec::from([0.; SAMPLE_RATE * SECONDS_OF_PLAYBACK]);
 
+    for (frame_index, frame) in buffer_vec.iter_mut().enumerate() {
+        let ampplitude_1 = get_sample_at_frequency(frame_index, 440.);
+        let amplitude_2 = get_sample_at_frequency(frame_index, 157.);
+        *frame = ampplitude_1 * amplitude_2;
+    }
+    
+    use rodio::buffer::SamplesBuffer;
+    let buffer = SamplesBuffer::new(1, 44100, buffer_vec);
     let sink = Sink::try_new(&stream_handle).unwrap();
-
-    // Add a dummy source of the sake of the example.
-    let source = SineWave::new(440.0)
-        .take_duration(Duration::from_secs_f32(2.0))
-        .amplify(0.20);
-
-    sink.append(source);
-
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
+    sink.append(buffer);
     sink.sleep_until_end();
 }
