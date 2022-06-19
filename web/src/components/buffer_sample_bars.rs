16a5b6b365e011a2a84ed3b3e22677bf4e3b8ffe
Austin Theriot <austinmtheriot@gmail.com>
2022-06-19 15:57:37 -0500

    make buffer visualization renders O(1) instead of O(n)

--- web/src/components/buffer_sample_bars.rs
+++ web/src/components/buffer_sample_bars.rs
@@ -1,11 +1,11 @@
 use crate::state::app_context::{AppContext, AppContextError};
-use std::ops::Mul;
+use std::{ops::Mul, sync::Arc};
 use yew::{function_component, html, prelude::*};
 
 /// This represents the number of elements to use when showing a buffer sample representation.
-/// 
+///
 /// This many divs is slow in development but shows no noticable when running in production mode.
-/// 
+///
 /// Other render methods to consider:
 /// - Use .svg path <-- this would probably actually be pretty straightforward?
 ///     - Just use straight vertical lines with a certain thickness?
@@ -13,17 +13,45 @@ use yew::{function_component, html, prelude::*};
 /// - Use css gradient
 const NUM_AUDIO_DATA_POINTS: usize = 100;
 
+/// Analyzes a buffer of raw audio sample data into ```NUM_AUDIO_DATA_POINTS``` number of samples,
+/// where each sample represents the peak from a chunk of the original audio.
+pub fn get_buffer_maxes(buffer: &Arc<Vec<f32>>) -> Vec<String> {
+    if buffer.is_empty() {
+        return Vec::new();
+    }
+
+    // buffer has audio data: get averages from the buffer
+    let iteration_group_size = buffer.len() / NUM_AUDIO_DATA_POINTS;
+    let maxes: Vec<String> = buffer
+        .chunks(iteration_group_size)
+        .map(|samples| {
+            let max = samples
+                .to_owned()
+                .into_iter()
+                .map(f32::abs)
+                .reduce(f32::max)
+                .unwrap()
+                .mul(100.0);
+            let formatted_max = format!("{:.1}", max);
+            formatted_max
+        })
+        .collect();
+
+    maxes
+}
+
 /// A renders a list of divs, the height of which match the
 /// amplitude of samples in the context's current buffer.
 #[function_component(BufferSampleBars)]
 pub fn buffer_sample_bars() -> Html {
     let app_context = use_context::<AppContext>().expect(AppContextError::NOT_FOUND);
-    
+    let buffer_maxes = &app_context.state_handle.buffer_maxes;
+
     // empty buffer
     if app_context.state_handle.buffer.data.is_empty() {
         return html! {
             <>
-                {(0..NUM_AUDIO_DATA_POINTS).into_iter().map(|_| {
+                {buffer_maxes.iter().map(|_| {
                     html!{
                         <div class="buffer-sample-bar empty" />
                     }
@@ -33,27 +61,13 @@ pub fn buffer_sample_bars() -> Html {
     }
 
     // buffer has audio data: display it
-    let iteration_group_size = app_context.state_handle.buffer.data.len() / NUM_AUDIO_DATA_POINTS;
-    let sample_averages: Vec<String> = app_context
-        .state_handle
-        .buffer
-        .data
-        .chunks(iteration_group_size)
-        .map(|samples| {
-            let sum = samples.iter().map(|sample| sample.abs()).sum::<f32>();
-            let average_percent = (sum / samples.len() as f32).mul(100.0).min(100.0).max(1.0);
-            let formatted_percent = format!("{:.1}", average_percent);
-            formatted_percent
-        })
-        .collect();
-
     html! {
         <>
-            {sample_averages.iter().map(|percent_string| {
+            {buffer_maxes.iter().map(|percent_string| {
                 html!{
-                    <div 
-                        class="buffer-sample-bar" 
-                        style={format!("transform: scaleY({}%);", percent_string)} 
+                    <div
+                        class="buffer-sample-bar"
+                        style={format!("transform: scaleY({}%);", percent_string)}
                     />
                 }
             }).collect::<Html>()}
--- web/src/state/app_state.rs
+++ web/src/state/app_state.rs
@@ -1,6 +1,7 @@
 use super::buffer_handle::BufferHandle;
 use crate::audio::buffer::Buffer;
 use crate::audio::stream_handle::StreamHandle;
+use crate::components::buffer_sample_bars::get_buffer_maxes;
 use crate::state::app_action::AppAction;
 use std::rc::Rc;
 use yew::Reducible;
@@ -9,6 +10,13 @@ use yew::Reducible;
 pub struct AppState {
     /// the currently loaded audio buffer
     pub buffer: Buffer,
+    /// a list with a set length of max amplitudes from the original audio buffer
+    /// this makes re-rendering the audio buffer visualization O(1) instead of O(n),
+    /// where n is the length of buffer samples.
+    /// 
+    /// The audio amlitudes range from 0.0 -> 100.0 and are formatted as strings to
+    /// the tens decimal place.
+    pub buffer_maxes: Vec<String>,
     /// a handle to the audio context stream (keeps audio playing & stops audio when dropped)
     pub stream_handle: Option<StreamHandle>,
     /// represents what portion of the audio buffer is currently selected
@@ -24,6 +32,7 @@ impl Reducible for AppState {
             let action = action.clone();
             match action {
                 AppAction::SetBuffer(buffer) => {
+                    next_state.buffer_maxes = get_buffer_maxes(&buffer);
                     next_state.buffer = Buffer::new(buffer);
                 }
                 AppAction::SetStreamHandle(stream_handle) => {
