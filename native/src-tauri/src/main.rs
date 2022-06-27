2fd2ad7956e79968a5f31e7c9dc18a0026ab1edb
Austin Theriot <austinmtheriot@gmail.com>
2022-06-26 21:09:15 -0500

    example working with emitting events and invoking fns from the front end

--- native/src-tauri/src/main.rs
+++ native/src-tauri/src/main.rs
@@ -3,16 +3,52 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::Mutex;
+use tauri::Manager;
+
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct ExamplePayload {
+    message: String,
+}
+
+#[derive(Default)]
+struct AppState {
+    count: Mutex<u32>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+fn increment(state: tauri::State<AppState>) {
+    println!("Received increment command!");
+    *state.count.lock().unwrap() += 1;
 }
 
 fn main() {
     let context = tauri::generate_context!();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AppState::default())
+        .invoke_handler(tauri::generate_handler![increment])
         .menu(tauri::Menu::os_default(&context.package_info().name))
+        .setup(|app| {
+            // listen to the `event-name` (emitted on any window)
+            let id = app.listen_global("event-name", |event| {
+                println!("got event-name with payload {:?}", event.payload());
+            });
+            
+            // unlisten to the event using the `id` returned on the `listen_global` function
+            // an `once_global` API is also exposed on the `App` struct
+            // app.unlisten(id);
+
+            // emit the `event-name` event to all webview windows on the frontend
+            app.emit_all(
+                "event-name",
+                ExamplePayload {
+                    message: "Tauri is awesome!".into(),
+                },
+            )
+            .unwrap();
+            Ok(())
+        })
         .run(context)
         .expect("error while running tauri application");
 }
--- native/src-ui/src/components/app.rs
+++ native/src-ui/src/components/app.rs
@@ -1,10 +1,44 @@
+use log::info;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use yew::{function_component, html, prelude::*};
+
+#[wasm_bindgen(module = "@tauri-apps/api/event")]
+extern "C" {
+    #[wasm_bindgen(js_name = emit)]
+    fn emit(event: String);
+    
+    #[wasm_bindgen(js_name = emit)]
+    fn emit_with_payload(event: String, arguments: JsValue);
+}
+
+#[wasm_bindgen(module = "@tauri-apps/api/tauri")]
+extern "C" {
+    #[wasm_bindgen(js_name = invoke)]
+    fn invoke(event: String);
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
+    use_effect_with_deps(move |_| {
+        info!("emitting event from the front-end");
+        emit(String::from("event-name"));
+        info!("invoking increment functin ofrom front-end");
+        invoke(String::from("increment"));
+        || {}
+    }, ());
+
     html! {
         <div>
-            {"Test"}
+            <button
+                onclick={|_| emit(String::from("event-name"))}
+            >
+                {"Emit event"}
+            </button>
+            <button
+                onclick={|_| invoke(String::from("increment"))}
+            >
+                {"Invoke command"}
+            </button>
         </div>
     }
 }
