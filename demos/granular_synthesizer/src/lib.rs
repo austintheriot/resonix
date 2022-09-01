462b0f0fae2c6cd98aae0bc88f7eb8061bb3385d
Austin Theriot <austinmtheriot@gmail.com>
2022-09-01 00:26:46 -0500

    remove wee_alloc

--- demos/granular_synthesizer/src/lib.rs
+++ demos/granular_synthesizer/src/lib.rs
@@ -6,14 +6,6 @@ pub mod icons;
 pub mod state;
 pub mod utils;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-// #[cfg(feature = "wee_alloc")]
-// #[global_allocator]
-// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
