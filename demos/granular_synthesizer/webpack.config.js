4499553a862a6755be10c946e1f18b48270bdfa5
Austin Theriot <austinmtheriot@gmail.com>
2023-05-21 21:48:25 -0500

    increased size limitations in webpack

--- demos/granular_synthesizer/webpack.config.js
+++ demos/granular_synthesizer/webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const distPath = path.resolve(__dirname, "dist");
 const staticFilesSrc = path.resolve(__dirname, "static");
 const audioFilesSrc = path.resolve(__dirname, "../../assets");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+  const isProduction = argv.mode === "production";
   return {
     devServer: {
       port: isProduction ? 8000 : 4000,
@@ -16,14 +16,14 @@ module.exports = (env, argv) => {
         directory: distPath,
       },
       historyApiFallback: {
-        index: '/'
+        index: "/",
       },
       open: true,
     },
     experiments: {
       syncWebAssembly: true,
     },
-    entry: './index.js',
+    entry: "./index.js",
     output: {
       path: distPath,
       filename: "main.js",
@@ -32,11 +32,7 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
       ],
     },
@@ -44,8 +40,8 @@ module.exports = (env, argv) => {
       new CopyPlugin({
         patterns: [
           { from: staticFilesSrc, to: distPath },
-          { from: audioFilesSrc, to: distPath }
-        ]
+          { from: audioFilesSrc, to: distPath },
+        ],
       }),
       new WasmPackPlugin({
         crateDirectory: __dirname,
@@ -54,6 +50,11 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
     ],
     mode: isProduction ? "production" : "development",
+    // ignore for now
+    // todo - consider asset size limitations
+    performance: {
+      maxEntrypointSize: Infinity,
+      maxAssetSize: Infinity,
+    },
   };
 };
-
