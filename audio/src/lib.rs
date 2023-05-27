f91cb731b3c1ffffc8d01ba291e990e4878d2000
Austin Theriot <austinmtheriot@gmail.com>
2023-05-27 14:43:01 -0500

    switch to using no hash hasher for some performance wins

--- audio/src/lib.rs
+++ audio/src/lib.rs
@@ -5,7 +5,6 @@ mod envelopes;
 pub mod grain;
 pub mod granular_synthesizer;
 pub mod granular_synthesizer_action;
-mod int_set;
 pub mod max;
 pub mod min;
 mod num_channels;
@@ -14,5 +13,4 @@ pub mod utils;
 
 pub use downmixers::*;
 pub use envelopes::*;
-pub use int_set::*;
 pub use num_channels::*;
