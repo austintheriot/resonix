38484ac245a5d5996a83098782895a6d99284fcf
Austin Theriot <austinmtheriot@gmail.com>
2023-06-01 17:06:38 -0500

    wip

--- resonix/src/audio_player/from_context.rs
+++ resonix/src/audio_player/from_context.rs
@@ -1,6 +1,7 @@
 use crate::AudioPlayerContext;
 
 /// Allows a function to pull whatever data it needs out of the audio Context
-pub trait FromContext {
-    fn from_context(context: &AudioPlayerContext) -> Self;
+pub trait FromContext<'a, D> {
+    // borrow of the 
+    fn from_context<'b: 'a>(context: &'b AudioPlayerContext<D>) -> Self;
 }
