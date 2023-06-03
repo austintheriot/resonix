5b80b9056607e05b501dc214a76ef7e1410f1b6d
Austin Theriot <austinmtheriot@gmail.com>
2023-06-02 22:13:05 -0500

    wip - context dropped while still borrowed

--- resonix/src/audio_player/from_context.rs
+++ resonix/src/audio_player/from_context.rs
@@ -1,7 +1,7 @@
 use crate::AudioPlayerContext;
 
 /// Allows a function to pull whatever data it needs out of the audio Context
-pub trait FromContext<'a, 'c: 'a, D> {
+pub trait FromContext<'a, D> {
     // borrow of the
-    fn from_context(context: &'c AudioPlayerContext<D>) -> Self;
+    fn from_context(context: &'a AudioPlayerContext<D>) -> Self;
 }
