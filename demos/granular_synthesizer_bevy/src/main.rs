a200f1edfb8875a27d59490eb801de8c50bdbbf8
Austin Theriot <austinmtheriot@gmail.com>
2023-06-06 23:43:00 -0500

    fix compiler errors after changing resolver

--- demos/granular_synthesizer_bevy/src/main.rs
+++ demos/granular_synthesizer_bevy/src/main.rs
@@ -2,13 +2,12 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        // .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .run();
 }
 
-fn setup() {
-    // let music = asset_server.load("sounds/Windless Slopes.ogg");
-    // audio.play(music);
-    println!("Hello, world!");
+fn setup(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    let music = asset_server.load("sounds/Windless Slopes.ogg");
+    audio.play(music);
 }
