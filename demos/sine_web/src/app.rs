3a9805ef583506b1389d3a4e585993f1708d7fb0
Austin Theriot <austinmtheriot@gmail.com>
2023-06-14 19:51:33 -0500

    add buttons to sine_web demo

--- demos/sine_web/src/app.rs
+++ demos/sine_web/src/app.rs
@@ -1,16 +1,29 @@
-use leptos::{component, view, IntoView, Scope, spawn_local};
+use leptos::{component, view, IntoView, Scope, spawn_local, create_signal, SignalSet};
 
 use crate::audio::set_up_audio_stream;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
-    spawn_local(async {
-        let audio_out = set_up_audio_stream().await;
-        let audio_out = Box::new(audio_out);
-        Box::leak(audio_out);
-    });
-    
+    let (_, set_audio) = create_signal(cx, None);
+
     view! { cx,
-        <p>"This is a sine wave!"</p>
+        <p>"Sine wave: "</p>
+        <button
+            on:click=move |_| {
+                spawn_local(async move {
+                    let audio_out = set_up_audio_stream().await;
+                    set_audio.set(Some(audio_out));
+                });
+            }
+        >
+            "Start"
+        </button>
+        <button
+            on:click=move |_| {
+              set_audio.set(None);
+            }
+        >
+            "Stop"
+        </button>
     }
 }
