909229565ccd3d6b3044d315757b05e1cca905d8
Austin Theriot <austinmtheriot@gmail.com>
2023-06-15 21:03:16 -0500

    optimize sine wave implementation

--- resonix/src/sine/sine_struct.rs
+++ resonix/src/sine/sine_struct.rs
@@ -2,12 +2,24 @@ use std::f32::consts::PI;
 
 use crate::SampleRate;
 
+/// Produces a sine wave at the given frequency and sample rate
+/// 
+/// e.g. at a sample_rate of `100` and a frequency of `1.0`, 
+/// `Sine` will complete a full wavelength cycle after `next_sample`
+/// has been called 100 times.
+/// 
+/// At a sample_rate of `44100` and a frequency of `440.0`, the 
+/// note `A` will be produced, so long as the sample_rate 
+/// given to sine matches the audio context's sample rate.
 #[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Default)]
 pub struct Sine {
     sample_rate: SampleRate,
     frequency: f32,
     phase: f32,
+    angular_frequency: f32,
 }
+
+const TWO_PI: f32 = 2.0 * PI;
 
 impl Sine {
     pub fn new() -> Self {
@@ -15,15 +27,17 @@ impl Sine {
             sample_rate: SampleRate::new(1u32),
             frequency: 0.0,
             phase: 0.0,
+            angular_frequency: 0.0,
         }
     }
 
     pub fn next_sample(&mut self) -> f32 {
-        let angular_frequency = 2.0 * PI * self.frequency / *self.sample_rate as f32;
         let sample = self.phase.sin();
 
-        // always wrap phase around
-        self.phase = (self.phase + angular_frequency) % (2.0 * PI);
+        self.phase += self.angular_frequency;
+
+        // always wrap phase around to be within unit circle
+        self.phase %= TWO_PI;
 
         sample
     }
@@ -33,12 +47,27 @@ impl Sine {
     }
 
     pub fn set_sample_rate(&mut self, sample_rate: impl Into<SampleRate>) -> &mut Self {
-        self.sample_rate = sample_rate.into();
+        let prev_sample_rate = self.sample_rate;
+        let sample_rate = sample_rate.into();
+
+        // only run calculations if necessary
+        if prev_sample_rate != sample_rate {
+            self.sample_rate = sample_rate.into();
+            self.angular_frequency = self.calculate_angular_frequency();
+        }
+
         self
     }
 
     pub fn set_frequency(&mut self, frequency: f32) -> &mut Self {
-        self.frequency = frequency;
+        let prev_frequency = self.frequency;
+
+        // only run calculations if necessary
+        if prev_frequency != frequency {
+            self.frequency = frequency;
+            self.angular_frequency = self.calculate_angular_frequency();
+        }
+
         self
     }
 
@@ -48,6 +77,10 @@ impl Sine {
 
     pub fn frequency(&self) -> f32 {
         self.frequency
+    }
+
+    fn calculate_angular_frequency(&self) -> f32 {
+        TWO_PI * self.frequency / *self.sample_rate as f32
     }
 }
 
