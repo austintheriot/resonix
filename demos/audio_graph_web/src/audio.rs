3abaafc87890cde5e979dd0f7c39c51900a99a71
Austin Theriot <austinmtheriot@gmail.com>
2023-06-21 13:58:38 -0500

    improve processing performance by caching connections

--- demos/audio_graph_web/src/audio.rs
+++ demos/audio_graph_web/src/audio.rs
@@ -1,4 +1,4 @@
-use resonix::{AudioContext, Connect, DACBuildError, DACNode, SineNode};
+use resonix::{AudioContext, Connect, DACBuildError, DACNode, SineNode, PassThroughNode, Node};
 
 pub async fn set_up_audio_graph() -> Result<AudioContext, DACBuildError> {
     let mut audio_context = AudioContext::new();
@@ -6,8 +6,20 @@ pub async fn set_up_audio_graph() -> Result<AudioContext, DACBuildError> {
 
     let sample_rate = audio_context.sample_rate().unwrap();
     let sine_node = SineNode::new_with_config(&mut audio_context, sample_rate, 440.0);
+
+    let pass_through_node = PassThroughNode::new(&mut audio_context);
+    sine_node.connect(&pass_through_node).unwrap();
+
+    let mut prev_node = pass_through_node;
+
+    for _ in 0..100 {
+        let pass_through_node = PassThroughNode::new(&mut audio_context);
+        prev_node.connect(&pass_through_node).unwrap();
+        prev_node = pass_through_node;
+    }
+
     let dac_node = DACNode::new(&mut audio_context);
-    sine_node.connect(&dac_node).unwrap();
+    prev_node.connect(&dac_node).unwrap();
 
     Ok(audio_context)
 }
