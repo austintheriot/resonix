9b8f6e05e9f4b90db310a42b6f045301d9c2e42c
Austin Theriot <austinmtheriot@gmail.com>
2025-02-19 20:20:09 -0600

    add some more traits

--- crates/resonix-traits/src/lib.rs
+++ crates/resonix-traits/src/lib.rs
@@ -1,10 +1,14 @@
 mod amplitude;
+mod audio_context;
+mod audio_context_options;
 mod audio_node;
 mod channel;
 mod frame;
 mod sample;
 
 pub use amplitude::*;
+pub use audio_context::*;
+pub use audio_context_options::*;
 pub use audio_node::*;
 pub use channel::*;
 pub use frame::*;
