8c1e7618d10d341e4cf4356eb82887b7f1af1993
Austin Theriot <austinmtheriot@gmail.com>
2025-05-12 21:01:20 -0700

    WIP - add some ideas for graph API

--- crates/resonix-graph/src/lib.rs
+++ crates/resonix-graph/src/lib.rs
@@ -1,5 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
-}
+#![no_std]
+extern crate alloc;
+
+mod resonix_audio_node;
+mod resonix_graph;
+mod resonix_param_node;
+
+pub use resonix_audio_node::*;
+pub use resonix_graph::*;
+pub use resonix_param_node::*;
---
+++ crates/resonix-graph/src/resonix_graph.rs
@@ -0,0 +1,48 @@
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use core::any::Any;
+
+use crate::{ResonixAudioNode, ResonixParamNode};
+
+pub enum Connectable {
+    AudioNode(Box<dyn ResonixAudioNode>),
+    ParamNode(Box<dyn ResonixParamNode>),
+    Graph(Box<dyn ResonixGraph>),
+}
+
+pub trait ResonixGraph {
+    fn add(self: &mut Self, connectable: Connectable);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn implementation_ideas() {
+        /*
+        // SIMPLEST OPTION, modelled after Web Audio API
+        let graph = Graph::new();
+        let node_a = NodeA::new();
+        let node_b = NodeB::new();
+        node_a.connect(node_b);
+        graph.add(node_a);
+
+        let graph = NodeA::new().connect(NodeB::new());
+
+        // USING WITH-SYNTAX-compose sub-grapsh
+        let mut graph = Graph::new();
+        graph.add_with(|| {
+            NodeA::new()
+                .connect(NodeB::new())
+                .connect(
+                    NodeC::new().connect(NodeD::new())
+                )
+        })
+
+        // USING HASH_MAP-like Result Data-types
+        graph
+            .add_node(SomeNode::new())
+            .connect_to(SomeOtherNode::new())
+
+         */
+    }
+}
