9087095881c3d9825ef8cacf7447f2e357ece189
Austin Theriot <austinmtheriot@gmail.com>
2026-03-11 21:20:40 -0500

    update comments

--- crates/resonix-graph/src/traits/audio_buffer.rs
+++ crates/resonix-graph/src/traits/audio_buffer.rs
@@ -1,15 +1,23 @@
 use crate::primitives::{AudioBufferError, Channel, Sample};
 
 pub trait AudioBuffer {
+    /// Returns the length of each slice per channel.
+    ///
+    /// For example, if an `AudioBuffer` implementation has 2 channels
+    /// and a block_size of `256`, then the total length of the
+    /// `AudioBuffer` would be assumed to be `512`.
     fn block_size(&self) -> usize;
 
+    /// Returns the number of channels in the audio buffer.
     fn channels(&self) -> usize;
 
-    /// Returns the samples for channel `c` (0-indexed).
+    /// Returns the samples for `channel` (0-indexed).
     ///
     /// Should return `Err(ChannelOutOfRange)` if `c >= self.channels()`.
     fn channel(&self, channel: impl Into<Channel>) -> Result<&[Sample], AudioBufferError>;
 
+    /// Returns an iterator over each channel in the audio buffer.
+    /// Iterates from the 1st channel (channel `0`) to the highest.
     fn channels_iter(&self) -> Result<impl Iterator<Item = &[Sample]>, AudioBufferError>;
 
     /// Returns the single channel's samples.
--- crates/resonix-graph/src/traits/audio_buffer_mut.rs
+++ crates/resonix-graph/src/traits/audio_buffer_mut.rs
@@ -1,6 +1,17 @@
 use crate::primitives::{AudioBufferError, Channel, Sample};
 
 pub trait AudioBufferMut: crate::traits::AudioBuffer {
+    /// Returns the mutable samples slice for `channel` (0-indexed).
+    ///
+    /// Should return `Err(ChannelOutOfRange)` if `c >= self.channels()`.
+    fn channel_mut(
+        &mut self,
+        channel: impl Into<Channel>,
+    ) -> Result<&mut [Sample], AudioBufferError>;
+
+    /// Returns an iterator over each channel in the audio buffer.
+    ///
+    /// Iterates from the 1st channel (channel `0`) to the highest.
     fn channels_iter_mut(
         &mut self,
     ) -> Result<impl Iterator<Item = &mut [Sample]>, AudioBufferError>;
@@ -9,14 +20,6 @@ pub trait AudioBufferMut: crate::traits::AudioBuffer {
     ///
     /// Should return `Err(NotMono)` if `channels != 1`.
     fn mono_mut(&mut self) -> Result<&mut [Sample], AudioBufferError>;
-
-    /// Returns the mutable samples for channel `c` (0-indexed).
-    ///
-    /// Should return `Err(ChannelOutOfRange)` if `c >= self.channels()`.
-    fn channel_mut(
-        &mut self,
-        channel: impl Into<Channel>,
-    ) -> Result<&mut [Sample], AudioBufferError>;
 
     /// Returns the internal audio buffer as a contiguous, mutable slice,
     /// without checking the number of channels it contains.
