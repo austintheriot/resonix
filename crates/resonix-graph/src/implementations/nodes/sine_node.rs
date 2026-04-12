782457da7ad13652ab209095872e781c0ba391b2
Austin Theriot <austinmtheriot@gmail.com>
2026-04-12 13:33:58 -0500

    maybe fix tests?

--- crates/resonix-graph/src/implementations/nodes/sine_node.rs
+++ crates/resonix-graph/src/implementations/nodes/sine_node.rs
@@ -143,7 +143,7 @@ impl Deref for SineNode {
 #[cfg(test)]
 mod tests {
     use alloc::vec::Vec;
-    use approx::assert_abs_diff_eq;
+    use approx::assert_relative_eq;
 
     use super::*;
     use crate::{
@@ -203,7 +203,7 @@ mod tests {
         result
             .into_iter()
             .zip(vec![expected_sine_value(frequency, DEFAULT_CURRENT_TIME)])
-            .for_each(|(result, expected)| assert_abs_diff_eq!(*result, *expected));
+            .for_each(|(result, expected)| assert_relative_eq!(*result, *expected));
     }
 
     #[test]
@@ -219,7 +219,7 @@ mod tests {
                 DEFAULT_FREQUENCY,
                 DEFAULT_CURRENT_TIME,
             )])
-            .for_each(|(result, expected)| assert_abs_diff_eq!(*result, *expected));
+            .for_each(|(result, expected)| assert_relative_eq!(*result, *expected));
     }
 
     #[test]
@@ -244,7 +244,7 @@ mod tests {
                 expected_sine_value(frequencies[2], DEFAULT_CURRENT_TIME),
                 expected_sine_value(frequencies[3], DEFAULT_CURRENT_TIME),
             ])
-            .for_each(|(result, expected)| assert_abs_diff_eq!(*result, *expected));
+            .for_each(|(result, expected)| assert_relative_eq!(*result, *expected));
     }
 
     #[test]
